c210b800d85c2cd67248bba5d1432dacd5cce3be
Julia Chanas <juulia.chanas@gmail.com>
2023-11-18 20:53:55 +0100

    Practise react

---
+++ src/components/List/List.js
@@ -0,0 +1,24 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column';
+
+const List = () => {
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Things to do <span>soon</span>
+        </h1>
+      </header>
+      <p className={styles.description}>
+        Interesting things I want to check out
+      </p>
+      <div className={styles.columns}>
+        <Column title='Book' icon='book' />
+        <Column title='Movies' icon='film' />
+        <Column title='Games' icon='gamepad' />
+      </div>
+    </div>
+  );
+};
+
+export default List;
