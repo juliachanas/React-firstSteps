ebbebef2de3c8e0322a0b40788677b4d70f3f41e
Julia Chanas <juulia.chanas@gmail.com>
2023-11-18 21:43:58 +0100

    Add handleSubmit to list.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,7 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import { useState } from 'react';
-import { useEffect } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
   const [columns, setColumns] = useState([
@@ -10,11 +10,13 @@ const List = () => {
     { id: 3, title: 'Games', icon: 'gamepad' },
   ]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setColumns([...columns, { id: 4, title: 'Test column' }]);
-    }, 2000);
-  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue('');
+  };
+
+  const [value, setValue] = useState('');
 
   return (
     <div className={styles.list}>
@@ -31,6 +33,14 @@ const List = () => {
           <Column key={column.id} title={column.title} icon={column.icon} />
         ))}
       </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button>Add column</button>
+      </form>
     </div>
   );
 };
