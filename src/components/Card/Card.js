b23a58a4fdc5b9ec3dde6b538a3f8b28e0d2473b
Julia Chanas <juulia.chanas@gmail.com>
2023-12-03 19:42:46 +0100

    Add isFavorite to cards in  initialState

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,12 @@
 import styles from './Card.module.scss';
 
 const Card = (props) => {
-  return <li className={styles.card}>{props.title}</li>;
+  return (
+    <li className={styles.card}>
+      {props.title}
+      <span className={'fa fa-star-o'} />
+    </li>
+  );
 };
 
 export default Card;
